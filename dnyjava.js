cd7f966f2c20f0cb747d27057cfe12ce6af9298f
Dương Ngọc Ý <duongocy@gmail.com>
2025-07-04 21:39:20 +0700

    aaaaaaaaa

--- dnyjava.js
+++ dnyjava.js
@@ -68,7 +68,7 @@ function update_invoice_list_to_table(invoices) {
             //hiển thị các thông tin cơ bản của hóa đơn 
             invoicecustomer.textContent = 'Customer name : '+hoadon.customer;    
             titleinvoice.textContent = 'Invoice Title : '+hoadon.invoice_title;
-            datecreate.textContent = 'Invoice Creating Date : ' + hoadon.invoice_date;    
+               
 
         //Lại dùng phương thức fetch để Lấy bảng chi tiết hóa đơn theo ID của hóa đơn
         const invoiceid = hoadon.invoice_id;
