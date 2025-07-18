968c1b008b060b03012bedc42294818be19e5215
Dương Ngọc Ý <duongocy@gmail.com>
2025-07-18 22:09:51 +0700

    xac thuc bang jwt 1

--- login-page.js
+++ login-page.js
@@ -90,7 +90,7 @@ login_button.addEventListener('click', async function (event) {
                     // Sau khi login thành công
                     sessionStorage.setItem('user_id', id);//gand giá trị id cho biến user_id và truyền đến phiên làm việc để trang được điều hướng tiếp theo có thể truy cập
                     sessionStorage.setItem('user_name', name);
-                    window.location.href = 'listofinvoice.html'; // Chuyển hướng đến trang hóa đơn
+                    window.location.href = 'index.html'; // Chuyển hướng đến trang hóa đơn
                 }
                 else {
                     alert("Email đã đăng ký trước đây.");
