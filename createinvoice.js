078b66414f0f0e0591f933c82859fef32d97097d
Y Duong Ngoc <ydn@hoya.com>
2025-07-28 14:23:57 +0700

    format lại trang thêm sản phâm 4

--- createinvoice.js
+++ createinvoice.js
@@ -18,13 +18,11 @@ let add_item_container = document.getElementById('add-item-to-invoice');
 let tao_invoice = document.getElementById('tao-invoice');
 
 //ẩn phần add item
-add_item_container.classList.add('hidden');
 
 //khai báo nút submit
 let submit_button = document.getElementById('submit-new-invoice-btn');
 
 //ẩn nút submit
-submit_button.classList.add('hidden');
 
 //Hàm cho nút nhấn Create New Invoice
 create_new_invoice_btn.addEventListener('click',function(){
@@ -36,11 +34,6 @@ create_new_invoice_btn.addEventListener('click',function(){
     if ((input_invoice_title.value.length!=0) && (input_invoice_customer.value.length!=0))
     {   
         //hiển thị lại các nội dung để thêm item 
-
-            add_item_container.classList.remove('hidden');
-            submit_button.classList.remove('hidden');
-            input_invoice_title.classList.add('hidden');
-            input_invoice_customer.classList.add('hidden');
             //tạo 1 mảng chứa các mục sản phẩm
             product_aray=[];
             product_object ={};
@@ -51,7 +44,6 @@ create_new_invoice_btn.addEventListener('click',function(){
             while(new_item_table_body.rows.length>0){
             new_item_table_body.deleteRow(0);
             //ẩn luôn nút tạo invoice
-            create_new_invoice_btn.classList.add('hidden');    
         }
         }
 
@@ -117,9 +109,6 @@ submit_button.addEventListener('click',function(){
             console.log('Lưu invoice thành công :', data);
             submit_button.textContent = "Save OK!"
             //hien thi lai 1 so thu : 
-            create_new_invoice_btn.classList.remove('hidden');
-            add_item_container.classList.add('hidden');
-            submit_button.classList.add('hidden');
             //xóa bảng item 
             while(new_item_table_body.rows.length>0){
                 new_item_table_body.deleteRow(0);
