6a43e4eafcb8a1b79548ee35816d82cf2372487a
Y Duong Ngoc <ydn@hoya.com>
2025-07-29 16:47:14 +0700

    format lại trang thêm sản phâm 9

--- listofinvoice.js
+++ listofinvoice.js
@@ -166,7 +166,8 @@ print_invoice_btn.addEventListener('click', async () => {
         filename: `${titleinvoice.textContent}.pdf`,
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+        pagebreak: { mode: 'avoid-all' }
     }).from(element).save();
     console.log("printed");
 });
