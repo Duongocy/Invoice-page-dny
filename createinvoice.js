75db48c3404f4a4fbfbcff3e2401f1ea80a3c1ab
Y Duong Ngoc <ydn@hoya.com>
2025-08-01 11:50:00 +0700

    format lại trang thêm sản phâm 13

--- createinvoice.js
+++ createinvoice.js
@@ -64,8 +64,8 @@ add_new_item_btn.addEventListener('click',function(){
         thutu = thutu+1;
         product_id_cell.textContent = thutu;
         product_name_cell.textContent = product_name.value;
-        product_price_cell.textContent = product_price.value;
-        product_quantity_cell.textContent = product_quantity.value;
+        product_price_cell.textContent = Number(product_price.value).toLocaleString;
+        product_quantity_cell.textContent = Number(product_quantity.value).toLocaleString;
         //nhập các giá trị vừa điền vào biến đối tượng (để lưu vào cơ sở dữ liệu)
         product_object["user_id"] = user_id;
         product_object["user_name"] = user_name;
