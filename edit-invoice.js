31e8c8b73107b542d84e445d876a0765f36705a8
Y Duong Ngoc <ydn@hoya.com>
2025-08-11 14:56:18 +0700

    format lại trang thêm sản phâm 18

--- edit-invoice.js
+++ edit-invoice.js
@@ -177,9 +177,9 @@ function update_add_item_table(data) {
         deletebtn.classList.add('delbtn');
         delete_btn.appendChild(deletebtn);
         deletebtn.addEventListener("click", function () { 
-             new_row.remove();
-            // console.log('Xóa hàng ',product_id_cell.textContent);
-            deleterow(product_id_cell.value-1, product_aray);
+            new_row.remove();
+            deleterow(product_id_cell.value - 1, product_aray);
+            update_add_item_table(product_aray);
         })
     });
 }
