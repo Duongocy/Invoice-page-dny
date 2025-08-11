7dde047c82112001b77c81d7ddf6169ed72b73d4
Dương Ngọc Ý <duongocy@gmail.com>
2025-08-11 22:28:22 +0700

    format lai trang new invoice 4

--- edit-invoice.js
+++ edit-invoice.js
@@ -180,7 +180,7 @@ function update_add_item_table(data) {
         delete_btn.appendChild(deletebtn);
         deletebtn.addEventListener("click", function () { 
             new_row.remove();
-            deleterow(Number(new_row.cells[0].textContent)-1, product_aray);
+            deleterow(Number(new_row.cells[0].textContent)-1, product_aray); //trừ 1 vì chỉ số phần từ bắt đầu từ số 0 còn chỉ số trên bảng từ 1
             thutu = 0;
             update_add_item_table(product_aray);
             // console.log('Chỉ số dòng : ', Number(new_row.cells[0].textContent));
